8eee530658d0589d49bd85c91fb547abde4b4b8a
Afnan9565 <98505187+Afnan9565@users.noreply.github.com>
2022-03-29 16:34:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function calc(){
+    var sum = parseInt(document.getElementById("maths").value) + parseInt(document.getElementById("sci").value) + parseInt(document.getElementById("eng").value) + parseInt(document.getElementById("hin").value) + parseInt(document.getElementById("ssci").value);
+    console.log(sum);
+    var percentage = sum / 2;
+    console.log(percentage);
+    var a = parseInt(document.getElementById("exper").value) / 100;
+    var totalmarksrequired = a * 400;
+    var marksneededmore = totalmarksrequired - sum;
+    var marksineachsubjects = marksneededmore / 5;
+    console.log(marksineachsubjects)
+    if(marksineachsubjects === undefined | marksineachsubjects === NaN){
+        document.getElementById("result").innerHTML = `Error! Please enter all fields`
+    }
+    else{
+        document.getElementById("result").innerHTML = `You need to score approximately ${marksineachsubjects} in each subject to reach your goal percentage when the weightage of term 1 is 50%`
+    }
+}
